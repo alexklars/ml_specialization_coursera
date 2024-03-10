00a26a408f7e965fb4cf19a3e3e62d252dd37f6f
Aliaksei Maiseyenkau <alex.moiseenkov1995@gmail.com>
2024-03-10 17:26:35 +0100

    Add logistic regression example (#2)
    * Add logistic regression example
    
    * Add scikit-learn LogisticRegression example
    
    * fix
    
    ---------
    
    Co-authored-by: aliakseimaiseyenkau <aliaksei.maiseyenkau@reedonline.co.uk>

---
+++ main/logistic_regression/logistic_regression_interactive_example.py
@@ -0,0 +1,16 @@
+# Lab: Gradient descent for logistic regression
+import matplotlib.pyplot as plt
+import numpy as np
+
+from common.plt_quad_logistic import plt_quad_logistic
+
+plt.style.use('./common/deeplearning.mplstyle')
+
+x_train = np.array([0., 1, 2, 3, 4, 5])
+y_train = np.array([0, 0, 0, 1, 1, 1])
+
+w_range = np.array([-1, 7])
+b_range = np.array([1, -14])
+
+# To see a plot, add a breakpoint and run debug
+quad = plt_quad_logistic(x_train, y_train, w_range, b_range)
